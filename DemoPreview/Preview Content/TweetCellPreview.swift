5beb0fc75d7d15324d6924a90ea104af2cd5e797
kenmaz <kentaro.matsumae@gmail.com>
2020-02-16 02:52:29 +0900

    add

---
+++ DemoPreview/Preview Content/TweetCellPreview.swift
@@ -0,0 +1,56 @@
+//
+//  TweetCellPreview.swift
+//  DemoPreview
+//
+//  Created by kenmaz on 2020/02/15.
+//  Copyright © 2020 kenmaz. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct TweetCellPreview: PreviewProvider {
+
+    struct Preview: UIViewRepresentable {
+        typealias UIViewType = PreviewContainer
+        let cell: TweetCell
+        init(_ input: TweetCell.Input) {
+            cell = TweetCell()
+            cell.apply(input: input)
+        }
+        func makeUIView(context: UIViewRepresentableContext<Preview>) -> UIViewType {
+            return PreviewContainer(view: cell.contentView)
+        }
+        func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<Preview>) {
+        }
+    }
+
+    static let text = "Hello, try! Swift Tokyo. Hello, try! Swift Tokyo"
+    static let longText = "Very long long long long long long long long long long long long long long Text"
+
+    static var previews: some View {
+        Group {
+            Preview(.init(message: text))
+                .previewDisplayName("Short text")
+            Preview(.init(message: longText))
+                .previewDisplayName("Long text")
+            Preview(.init(message: text, hasPicture: true))
+                .previewDisplayName("With picture")
+            Preview(.init(message: text))
+                .environment(\.colorScheme, .dark)
+                .previewDisplayName("Dark mode")
+            Preview(.init(message: text))
+                .environment(\.sizeCategory, .accessibilityLarge)
+                .previewDisplayName("Large text")
+            Preview(.init(message: text))
+                .environment(\.sizeCategory, .extraSmall)
+                .previewDisplayName("Small text")
+            Preview(.init(message: "مرحبا ، حاول! سويفت طوكيو."))
+                .environment(\.layoutDirection, .rightToLeft)
+                .previewDisplayName("Right to left")
+
+        }.previewLayout(.fixed(width: 320, height: 180))
+    }
+
+    static var platform: PreviewPlatform? = .iOS
+}
