5f676c283bb45f10027e325faeffd79c7527fca3
kenmaz <kentaro.matsumae@gmail.com>
2020-02-16 14:05:12 +0900

    Add hasPicture models

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -11,14 +11,14 @@ import UIKit
 class ViewController: UITableViewController {
 
     var tweets: [Tweet] = [
-        Tweet(message: "Hello, try! Swift Tokyo", isFavorited: false),
-        Tweet(message: "Hello, iOS Developer", isFavorited: false),
-        Tweet(message: "I'm sleepy", isFavorited: true),
-        Tweet(message: "Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text ", isFavorited: false),
-        Tweet(message: "Hello, try! Swift Tokyo", isFavorited: false),
-        Tweet(message: "Hello, iOS Developer", isFavorited: false),
-        Tweet(message: "I'm sleepy", isFavorited: true),
-        Tweet(message: "Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text ", isFavorited: false),
+        Tweet(message: "Hello, try! Swift Tokyo"),
+        Tweet(message: "Hello, iOS Developer"),
+        Tweet(message: "I'm sleepy"),
+        Tweet(message: "Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text "),
+        Tweet(message: "Hello, try! Swift Tokyo", hasPicture: true),
+        Tweet(message: "Hello, iOS Developer", hasPicture: true),
+        Tweet(message: "I'm sleepy", hasPicture: true),
+        Tweet(message: "Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text Long Text ", hasPicture: true),
     ]
 
     override func viewDidLoad() {
